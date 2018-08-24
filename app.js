d48b6e2dd48465e9702bdaf17cecf11f0818bd92
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-23 23:04:17 -0500

    add src code

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Starting our Application');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFileSync('messages.txt', '\n' + `Hello ${user.username}a` + '!');
