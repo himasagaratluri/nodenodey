930256d510a84f79877f0177882a805eab0fc45f
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-25 16:47:17 -0500

    comment out the experimented lines of code

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ console.log('Starting our Application');
 const fs = require('fs');
 const os = require('os');
 var note = require('./note.js');
-
-var sum = note.add();
-
-console.log(sum);
-
-console.log('Here is another way of using functions');
-console.log('+------------------------------+');
-console.log('| Result: '+ note.newAdd(10,10)+' ------------------|');
-console.log('+------------------------------+');
+const _  = require('lodash');
 
 
+
+
+
+// var sum = note.add();
+// console.log(sum);
+// console.log('Here is another way of using functions');
+// console.log('+------------------------------+');
+// console.log('| Result: '+ note.newAdd(10,10)+' ------------------|');
+// console.log('+------------------------------+');
 // var user = os.userInfo();
-
 // fs.appendFileSync('messages.txt', '\n' + `Hello ${user.username}a` + ` You are ${note.age} old !`+ '!');
