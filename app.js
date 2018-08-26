6855a499f917c6fe7a403f98068685700a05c4ea
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-26 00:05:16 -0500

    added if else statements to recognize user i/o

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const fs = require('fs');
 const _  = require('lodash');
 //OwnModules
 var note = require('./note.js');
+
+var command = process.argv[2];
+
+if (command === 'read') {
+    console.log('Reading the notes from the DataBase: ');
+} else if (command === 'remove'){
+    console.log('Removing the notes from the DataBase: ');
+} else{
+    console.log('Command is not recognized ')
+}
