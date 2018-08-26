ba163b889385c3c9848346cea91ac89319bd1bdf
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-26 00:09:24 -0500

    if else loops

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ if (command === 'read') {
     console.log('Reading the notes from the DataBase: ');
 } else if (command === 'remove'){
     console.log('Removing the notes from the DataBase: ');
-} else{
+} else if (command === 'list'){
+    console.log('Listing all the notes from the DataBase:');
+}else if (command === 'add'){
+    console.log('Adding note to the DataBase');
+}else{
     console.log('Command is not recognized ')
 }
