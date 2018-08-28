f0e4d06c8d92a336ec7e47d20a40a8732c05a4d8
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-27 22:59:55 -0500

    nothing new

--- innovation/json.js
+++ innovation/json.js
@@ -6,3 +6,10 @@
 
 // console.log(typeof stringObj);
 // console.log(stringObj);
+
+var personString = '{"Name": "HimaSagarAtluri", "age" : "26"}';
+var person = JSON.parse(personString);
+console.log(typeof personString);
+console.log(typeof person);
+console.log(person);
+console.log(personString);
