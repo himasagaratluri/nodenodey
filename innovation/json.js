699973a8ced5ed48d4fd279f2bd8610a41b7a2b7
himasagar.atluri@gmail.com <himasagar.atluri@gmail.com>
2018-08-30 21:57:50 -0500

    create json file

--- innovation/json.js
+++ innovation/json.js
@@ -7,9 +7,27 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"Name": "HimaSagarAtluri", "age" : "26"}';
-var person = JSON.parse(personString);
-console.log(typeof personString);
-console.log(typeof person);
-console.log(person);
-console.log(personString);
+// var personString = '{"Name": "HimaSagarAtluri", "age" : "26"}';
+// var person = JSON.parse(personString);
+// console.log(typeof personString);
+// console.log(typeof person);
+// console.log(person);
+// console.log(personString);
+
+const fs = require('fs');
+
+var originalNote = {
+    title: 'title yo',
+    body: 'this the body yo'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
